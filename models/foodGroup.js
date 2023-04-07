5535d53c059207b311198cf05cb3c4e2d6b56c50
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-07 13:04:38 -0700

    Create food group model

---
+++ models/foodGroup.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const FoodGroupSchema = new Schema({
+    name: { type: String, required: true, maxLength: 100 },
+    description: { type: String, required: true },
+});
+
+// Virtual Properties
+// Food Group URL
+FoodGroup.virtual("url").get(function() {
+    return `/home/foodgroup/${this.id}`;
+});
+
+// Export model
+module.exports = mongoose.model("FoodGroup", FoodGroupSchema);
