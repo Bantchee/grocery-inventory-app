0ee46ef7aa0e59275837a111a11e44b85338cfbb
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-07 13:29:33 -0700

    Create food model

---
+++ models/food.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const Foodschema = new Schema({
+    name: { type: String, required: true, maxLength: 100 },
+    description: { type: String, required: true},
+    food_group: { type: Schema.Types.ObjectId, ref: 'Food Group'},
+});
+
+// Virtual Properties
+// Food URL
+Food.virtual('url').get(function () {
+    return `/home/food/${this.id}`;
+});
+
+// Export model
+module.exports = mongoose.model('Food', FoodSchema);
