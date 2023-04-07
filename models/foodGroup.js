cb5e8d16c32a2fb9054b0ecdf5bb3c1ce1ad2086
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-07 14:33:48 -0700

    Update food group model

--- models/foodGroup.js
+++ models/foodGroup.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 const FoodGroupSchema = new Schema({
     name: { type: String, required: true, maxLength: 100 },
-    description: { type: String, required: true },
 });
 
 // Virtual Properties
