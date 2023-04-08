2dd758faaab4cb51819eda6b0d0919f9e07d195e
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-08 12:42:45 -0700

    Implement index view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,37 @@
-exports.index = (req, res) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+const Store = require("../models/store");
+const Farm = require("../models/farm");
+const Food = require("../models/food");
+const FoodItem = require("../models/foodItem");
+const FoodGroup = require("../models/foodGroup");
+
+const async = require('async');
+
+exports.index = (req, res, next) => {
+    async.parallel(
+        {
+            store_count(callback) {
+                Store.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+            },
+            farm_count(callback) {
+                Farm.countDocuments({}, callback);
+            },
+            food_count(callback) {
+                Food.countDocuments({}, callback);
+            },
+            food_item_count(callback) {
+                FoodItem.countDocuments({}, callback);
+            },
+            food_group_count(callback) {
+                FoodGroup.countDocuments({}, callback);
+            },
+        },
+        (err, results) => {
+            res.render("index", {
+                title: "Grocery Inventory Home",
+                error: err,
+                data: results,
+            })
+        }
+    )
 };
   
