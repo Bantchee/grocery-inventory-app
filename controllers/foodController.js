e5b5c41fddb7c9b3d34c5229af17040810d619f1
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-19 09:05:11 -0700

    Implement food create view

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,5 +1,8 @@
 const Food = require("../models/food");
+const FoodGroup = require("../models/foodGroup");
 const async = require("async");
+const { body, validationResult } = require('express-validator');
+const luxon = require('luxon');
 
 // Display list of all Foods.
 exports.food_list = (req, res, next) => {
@@ -44,13 +47,79 @@ exports.food_detail = (req, res, next) => {
 
 // Display Food create form on GET.
 exports.food_create_get = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Food create GET");
+    // Get all food groups, which we can use to add to store.
+    async.parallel(
+        {
+            foodGroups(callback) {
+                FoodGroup.find(callback);
+            },
+        },
+        (err, results) => {
+            if(err) {
+                next(err);
+            }
+            res.render("food_form", {
+                title: "Create Food",
+                foodGroups: results.foodGroups,
+            });
+        }
+    )
 };
 
 // Handle Food create on POST.
-exports.food_create_post = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Food create POST");
-};
+exports.food_create_post = [
+    // Validate and sanitize fields.
+    body("name", "Name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Description must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("foodGroup")
+        .escape(),
+
+    // Process request after validation and sanitization.
+  (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a food group object with escaped and trimmed data.
+        const food = new Food({
+            name: req.body.name,
+            description: req.body.description,
+            food_group: req.body.foodGroup,
+        });
+
+        if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values and error messages.
+        FoodGroup.find().exec(function (err, foodGroups) {
+            if (err) {
+                return next(err);
+            }
+            // Successful, so render.
+            res.render("food_form", {
+                title: "Create Food",
+                foodGroups: foodGroups,
+                selected_food_group: food.food_group._id,
+                errors: errors.array(),
+                food,
+            });
+        });
+        return;
+        }
+
+        // Data from form is valid. Save store.
+        food.save((err) => {
+            if (err) {
+                return next(err);
+            }
+                // Successful: redirect to new store record.
+                res.redirect(food.url);
+        });
+    },
+];
 
 // Display Food delete form on GET.
 exports.food_delete_get = (req, res, next) => {
